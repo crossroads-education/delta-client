7fb04c0b66223baa1849b5db7d397470f4e14d80
Brian Huntley <bboy300@comcast.net>
2018-04-18 18:33:11 -0400

    Initial implementation of new rendering method to support objects and arrays

--- lib/DynamicComponent.ts
+++ lib/DynamicComponent.ts
@@ -27,48 +27,95 @@ export default abstract class DynamicComponent<P> extends Component {
 
     // pass in one or all of the state variables and render them
     public update(props: Partial<P>): void {
-        for (const key in props) this.props[key] = props[key]; // only update what changed
-        this.view = this.template(this.props);
-        this.render();
+        this.view = this.template(props);
+        for (const key in props) {
+            this.updateView(props[key], this.context, undefined, key);
+            this.props[key] = props[key];
+        }
+         // only update what changed
     }
 
     // call instead of $(document).ready and load initial content
     public async load(props?: Partial<P>): Promise<void> {
-        if (props) {
-            this.update(props);
-        } else {
-            this.render();
-            throw new Error("Missing properties, cannot update viewmodel variables");
-        }
+        this.render(props);
     }
 
     // render the component's view
-    protected render(): void {
+    protected render(props?: Partial<P>): void {
         // if the component is already rendered, only update what changed
+        /*
         if (this.context && $(this.container).find(this.context).length) {
             const newView = $(this.view);
             for (const key in this.props) {
-                let newElement = newView.find('[d-bind*="' + key + '"]');
-                let oldElement = (this.context.find('[d-bind*="' + key + '"]'));
-                if (oldElement.length === 0) {
-                    oldElement = this.context.closest('[d-bind*="' + key + '"]');
-                    newElement = newView.closest('[d-bind*="' + key + '"]');
-                }
-                if (oldElement[0]) {
-                    const directChild: Node = oldElement[0].childNodes[0];
-                    if (directChild && directChild.nodeValue) directChild.nodeValue = newElement[0].childNodes[0].nodeValue;
-                    $.each(oldElement[0].attributes, (index, attribute) => {
-                        if (newElement.attr(attribute.name)) {
-                            oldElement.attr(attribute.name, newElement.attr(attribute.name));
-                        }
-                    });
-                }
+
 
             }
         } else {
             // render content normally and set context to reference its own location
             this.context = $(this.view);
             $(this.container).append(this.context);
+        }
+        */
+        if (props) {
+            this.view = this.template(this.props);
+            this.props =  props;
+            this.context = $(this.view);
+            $(this.container).append(this.context);
+        } else {
+            throw new Error("Missing properties, cannot update viewmodel variables");
+        }
+
+    }
+
+    protected updateView(value: any, container: JQuery, index?: number, key?: any): void {
+        if (value.constructor.name === "Array") {
+            if ((<any>this.props[key]).length !== value.length) {
+                const elements = container.find('[d-bind*="' + key + '"]');
+                const newElements = $(this.view).find('[d-bind*="' + key + '"]');
+                newElements.each((index, newElement) => {
+                    if (index < ((<any>this.props[key]).length)) {
+                        this.updateView(value[index], $(elements).eq(index).parent(), index, key);
+                    } else {
+                        container.append(newElement);
+                    }
+                });
+
+                if (elements.length > newElements.length) {
+                    for (let i = newElements.length; i < elements.length; i++) {
+                        elements.eq(i).remove();
+                    }
+                }
+            } else {
+                value.forEach((val: any, index: number) => {
+                    this.updateView(val, container.find('[d-bind*="' + key + '"]').parent(), index, key);
+                });
+            }
+        } else if (value.constructor.name === "Object") {
+            $(container).find('[d-bind*="' + key + '"]').each((index, element) => {
+                for (const childKey in value) {
+                    this.updateView(value[childKey], $(element), undefined, childKey);
+                }
+
+            });
+
+        } else {
+            if (container.length > 0) {
+                let oldElements = container.find('[d-bind*="' + key + '"]');
+                if (oldElements.length === 0) {
+                    oldElements = container.closest('[d-bind*="' + key + '"]');
+                }
+                if (oldElements.length !== 0) {
+                    oldElements.each((index, element) => {
+                        const elementContent: Node = element.childNodes[0];
+                        if (elementContent && elementContent.nodeValue) elementContent.nodeValue = value;
+                        $.each(element.attributes, (index, attribute) => {
+                            if (attribute.name === key) {
+                                $(element).attr(key, value);
+                            }
+                        });
+                    });
+                }
+            }
         }
     }
 
