59fbc8a0870f167adf3595ed959374fc9d2a67c2
Brian Huntley <bboy300@comcast.net>
2018-04-19 15:04:20 -0400

    Rewrite renderer for delta to support arrays, arrays of objects, objects, and arrays inside of objects in the viewmodel

--- lib/DynamicComponent.ts
+++ lib/DynamicComponent.ts
@@ -42,22 +42,8 @@ export default abstract class DynamicComponent<P> extends Component {
 
     // render the component's view
     protected render(props?: Partial<P>): void {
-        // if the component is already rendered, only update what changed
-        /*
-        if (this.context && $(this.container).find(this.context).length) {
-            const newView = $(this.view);
-            for (const key in this.props) {
-
-
-            }
-        } else {
-            // render content normally and set context to reference its own location
-            this.context = $(this.view);
-            $(this.container).append(this.context);
-        }
-        */
         if (props) {
-            this.view = this.template(this.props);
+            this.view = this.template(props);
             this.props =  props;
             this.context = $(this.view);
             $(this.container).append(this.context);
@@ -65,57 +51,75 @@ export default abstract class DynamicComponent<P> extends Component {
             throw new Error("Missing properties, cannot update viewmodel variables");
         }
 
+
     }
-
-    protected updateView(value: any, container: JQuery, index?: number, key?: any): void {
+    // two-way data-binder, supports handlebars arrays, objects, and primitives
+    /*   Value: value of the property of the object you're currently iterating over, such as props[name], or an array of names, or an object name: {first: string, last: string}
+     *   Container: containing parent element, begins at context, refines as object searching becomes more defined
+     *   arrayIndex: if we're iterating over an array we must support passing the arrayIndex, arrays are updated one value at a time
+     *   key: current key of property you're iterating over. will always be a string.
+     */
+    protected updateView(value: any, container: JQuery, arrayIndex?: number, key?: any): void {
+        const boundProperty = '[d-bind*="' + key + '"]';
         if (value.constructor.name === "Array") {
-            if ((<any>this.props[key]).length !== value.length) {
-                const elements = container.find('[d-bind*="' + key + '"]');
-                const newElements = $(this.view).find('[d-bind*="' + key + '"]');
-                newElements.each((index, newElement) => {
-                    if (index < ((<any>this.props[key]).length)) {
-                        this.updateView(value[index], $(elements).eq(index).parent(), index, key);
-                    } else {
-                        container.append(newElement);
-                    }
+            const parent = container.find(boundProperty).parent();
+            if ((<any>this.props)[key].length === value.length) { // check if our new property is the same length as our parent
+                value.forEach((val: any, index: number) => { // iterate over each value in array of values
+                    // send current value in arrray, the parent dom element, index of array, and key value
+                    this.updateView(val, parent, index, key);
                 });
-
-                if (elements.length > newElements.length) {
-                    for (let i = newElements.length; i < elements.length; i++) {
-                        elements.eq(i).remove();
-                    }
-                }
-            } else {
-                value.forEach((val: any, index: number) => {
-                    this.updateView(val, container.find('[d-bind*="' + key + '"]').parent(), index, key);
-                });
+                return;
             }
-        } else if (value.constructor.name === "Object") {
-            $(container).find('[d-bind*="' + key + '"]').each((index, element) => {
-                for (const childKey in value) {
-                    this.updateView(value[childKey], $(element), undefined, childKey);
+            // if our value is not equal length we have to do some checks
+            const elements = container.find(boundProperty); // find all current elements with that key as a bound property
+            if ((<any>this.props)[key].length > value.length) { // size of array has shrunk from current size, must remove extra elements and change current ones
+                for (let i = (<any>this.props)[key].length; i < value.length; i++) {
+                    // remove extra elements from DOM
+                    elements.eq(i).remove();
                 }
-
+            }
+            for (let i = 0; i < value.length; i++) { // iterate through new values
+                if (i < (<any>this.props)[key].length) { // check if we're a new element, because we're out of bounds of our current props
+                    this.updateView(value[arrayIndex], parent, arrayIndex, key); // if not we update with the array index
+                } else {
+                    // if we are a new element we pull it from the new rendered DOM and append it to the containing element
+                    let newElement = $(this.view).find(boundProperty);
+                    if (!newElement.length) newElement = $(this.view).closest(boundProperty);
+                    container.append(newElement);
+                }
+            }
+            return;
+        }
+        if (value.constructor.name === "Object") { // property of view model is an object
+            let parentElement = container.find(boundProperty); // find containing element
+            if (parentElement.length > 1 || parentElement.length === 0) { // if theres more than one then we assume that we have an array index, i.e. an array of objects.
+                parentElement = container;
+            }
+            for (const childKey in value) {
+                this.updateView(value[childKey], parentElement, arrayIndex, childKey); // iterate through keys of object, arrayIndex will be null if we're not an array of objects
+            }
+            return;
+        }
+        if (container.length > 0) {
+            let element = container.find(boundProperty);
+            let newElement = $(this.view).find(boundProperty);
+            if (element.length === 0) { // if we're not inside the container, we must be the container
+                element = container;
+            }
+            if (newElement.length === 0) {
+                newElement = $(this.view).closest(boundProperty); // assume the the same with the newElement. we're at top level.
+            }
+            if (arrayIndex) { // if we're an array of objects, we use the dom rendered element to find which node to replace
+                element = element.eq(arrayIndex);
+                newElement = newElement.eq(arrayIndex);
+            }
+            const elementContent: Node = element[0].childNodes[0]; // get text content
+            if (elementContent && elementContent.nodeValue) elementContent.nodeValue = newElement[0].childNodes[0].nodeValue; // if we have text, replace the text
+            $.each(element[0].attributes, (index, attribute) => { // replace all attributes of the element with the new properties, if they exist as a dom rendered property
+                if (attribute.name === "data-" + key) {
+                    $(element).attr("data-" + key, value);
+                }
             });
-
-        } else {
-            if (container.length > 0) {
-                let oldElements = container.find('[d-bind*="' + key + '"]');
-                if (oldElements.length === 0) {
-                    oldElements = container.closest('[d-bind*="' + key + '"]');
-                }
-                if (oldElements.length !== 0) {
-                    oldElements.each((index, element) => {
-                        const elementContent: Node = element.childNodes[0];
-                        if (elementContent && elementContent.nodeValue) elementContent.nodeValue = value;
-                        $.each(element.attributes, (index, attribute) => {
-                            if (attribute.name === key) {
-                                $(element).attr(key, value);
-                            }
-                        });
-                    });
-                }
-            }
         }
     }
 
